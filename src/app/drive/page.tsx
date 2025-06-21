0508320c4996c43b1edef3859b2c2805fa327faa
Avanish Davuluri <avanishdavuluri@gmail.com>
2025-06-20 17:15:55 -0700

    Add mock drive

---
+++ src/app/drive/page.tsx
@@ -0,0 +1,10 @@
+import FileManager from "@/components/file-manager";
+
+export default function DrivePage() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-center">
+            <FileManager />
+        </main>
+    )
+
+}
