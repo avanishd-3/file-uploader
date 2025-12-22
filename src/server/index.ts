dde8bbbe2991e7d61000e0f4354849a7ec646323
Avanish Davuluri <avanishdavuluri@gmail.com>
2025-12-21 19:24:44 -0800

    refactor: remove unused eslint disable

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,5 +2,4 @@ import { env } from "@/env";
 // Project will not build if any env var is missing, so no need for extra checks here
 // Upload dir also has a default value in env.js
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 export const uploadDir: string = env.UPLOAD_DIR;
